835d6cc3fa87c0bafdd3d8ee70dacfc81f7b2e85
Tristan Leblanc <tristan@moosefactory.eu>
2016-03-12 14:17:44 +0100

    Initial Commit

---
+++ MFFoundation/MFFoundation.h
@@ -0,0 +1,13 @@
+//
+//  MFFoundation.h
+//  MFFoundation
+//
+//  Created by Tristan Leblanc on 12/03/16.
+//  Copyright © 2016 MooseFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFFoundation : NSObject
+
+@end
---
+++ MFFoundation/MFFoundation.m
@@ -0,0 +1,13 @@
+//
+//  MFFoundation.m
+//  MFFoundation
+//
+//  Created by Tristan Leblanc on 12/03/16.
+//  Copyright © 2016 MooseFactory. All rights reserved.
+//
+
+#import "MFFoundation.h"
+
+@implementation MFFoundation
+
+@end
